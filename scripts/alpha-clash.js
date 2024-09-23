6bcfbdf79ba7d32745ccccb8cfebc10e9922f3d0
rumiaktar <ce.rumiakter@gmail.com>
2024-09-23 23:02:24 +0600

    and and remove bg color pressed right key

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -7,12 +7,37 @@
 //     playGround.classList.remove('hidden');
 // }
 
+function handleKeyboardButtonPress(event){
+    const playerPressed = event.key;
+    console.log('Player Pressed', playerPressed);
+
+    // get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log (playerPressed, expectedAlphabet);
+
+    // check matched or not
+    if(playerPressed === expectedAlphabet){
+        console.log('you get a point');
+        console.log('you have pressed correctly', expectedAlphabet);
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('you missed, you lost a life');
+    }
+}
+
+// capture keyboard key press
+document.addEventListener('keyup', handleKeyboardButtonPress); 
+
 function continueGame(){
  const alphabet = getARandomAlphabet();
- console.log(alphabet);
+//  console.log(alphabet);
 
- const newAlphabet = document.getElementById('current-alphabet');
- newAlphabet.innerText = alphabet;
+ const currentAlphabet = document.getElementById('current-alphabet');
+ currentAlphabet.innerText = alphabet;
 
  setBackgroundColorById(alphabet);
 }
